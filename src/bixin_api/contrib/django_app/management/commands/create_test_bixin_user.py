e8563e1206444bc4870948d82c6e98e60b127a95
winkidney <winkidney@gmail.com>
2018-06-06 04:45:59 -0700

    Fix: Use new version of user-transfer

--- src/bixin_api/contrib/django_app/management/commands/create_test_bixin_user.py
+++ src/bixin_api/contrib/django_app/management/commands/create_test_bixin_user.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
 
@@ -6,11 +7,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         from bixin_api.contrib.django_app.models import BixinUser
-        BixinUser.objects.create(
-            username="hi-iam-user",
-            target_id='111111',
-            openid='1111111'
+
+        BixinUser.objects.get_or_create(
+            openid='1111111',
+            defaults=dict(
+                username="hi-iam-user",
+                target_id='111111',
+            )
         )
+        user = get_user_model()
+        user.objects.create_superuser(username='hallow', password="111111", email='a@b.com')
         self.stdout.write(
             self.style.SUCCESS("succeed")
         )
