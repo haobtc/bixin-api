a5ce660b6115e6e9bcefc718a2b6a2e106044e5b
winkidney <winkidney@gmail.com>
2018-06-10 20:38:51 -0700

    Fix: Won't import models in api.py

--- src/bixin_api/contrib/django_app/api.py
+++ src/bixin_api/contrib/django_app/api.py
@@ -1,10 +1,8 @@
-import logging
 from decimal import Decimal
 
 from bixin_api.client import PubAPI
 
 from .config import get_client
-from .models import Deposit, BixinUser, Withdraw
 
 
 def get_vendor_address(symbol):
@@ -17,6 +15,9 @@ def mk_transfer_in(user_id, symbol, amount, address=None):
     """
     数字货币还款/添加质押物
     """
+    from bixin_api.contrib.django_app.models import Deposit
+    from bixin_api.contrib.django_app.models import BixinUser
+
     if address is None:
         address = get_vendor_address(symbol)
     deposit = Deposit.objects.create(
@@ -29,6 +30,9 @@ def mk_transfer_in(user_id, symbol, amount, address=None):
 
 
 def mk_transfer_out(user_id, symbol, amount):
+    from bixin_api.contrib.django_app.models import Withdraw
+    from bixin_api.contrib.django_app.models import BixinUser
+
     withdraw = Withdraw.objects.create(
         address=None,
         symbol=symbol,
@@ -42,6 +46,7 @@ def get_transfer_status(order_id):
     """
     :returns: 'SUCCESS' or 'PENDING'
     """
+    from bixin_api.contrib.django_app.models import Deposit
     deposit = Deposit.objects.get(order_id=order_id)
     return deposit.status
 
