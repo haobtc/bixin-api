c0c4660ff09adcf9f70d76c5dc2ff6bb314932f9
winkidney <winkidney@gmail.com>
2018-07-16 10:23:18 +0800

    Fix: Use new open-id example

--- src/bixin_api/contrib/django_app/management/commands/create_test_bixin_user.py
+++ src/bixin_api/contrib/django_app/management/commands/create_test_bixin_user.py
@@ -1,19 +1,23 @@
 from django.core.management.base import BaseCommand
 
 
+def create_bixin_user():
+    from bixin_api.contrib.django_app.models import BixinUser
+
+    user, created = BixinUser.objects.get_or_create(
+        openid='openid_example',
+        defaults=dict(
+            username="hi-iam-user",
+            target_id='111111',
+        )
+    )
+    return user
+
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
     def handle(self, *args, **options):
-        from bixin_api.contrib.django_app.models import BixinUser
-
-        BixinUser.objects.get_or_create(
-            openid='1111111',
-            defaults=dict(
-                username="hi-iam-user",
-                target_id='111111',
-            )
-        )
+        create_bixin_user()
         self.stdout.write(
             self.style.SUCCESS("succeed")
         )
